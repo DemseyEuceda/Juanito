91dbcd70f275bb0e3cfd33df42fa598360f41b2d
Delmerespinal24 <Delmerespinal2@gmail.com>
2024-07-18 07:36:51 -0600

    prueba leer api

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -46,6 +48,58 @@ builder.Services.AddAuthorization();
 
 app.UseAuthentication();
   */
+
+/*
+ 
+ class program
+    {
+        static void Main(string[] args)
+        {
+            //GET-
+            using (var client = new HttpClient())
+            {
+                string url = "https://jsonplaceholder.typicode.com/posts/1";
+
+                client.DefaultRequestHeaders.Clear();
+                // client.DefaultRequestHeaders.Add("Autorization", "LKASJFKDLFDSAKJ");
+
+                var response = client.GetAsync(url).Result;
+
+                var res = response.Content.ReadAsStringAsync().Result;
+                dynamic json = JObject.Parse(res);
+
+                Console.WriteLine(json);
+                //-GET
+                //POST-
+                string url2 = "https://jsonplaceholder.typicode.com/posts";
+
+                // Crea el objeto JSON a enviar
+                var parametros = new
+                {
+                    title = "juanito",
+                    body = "Prueba Post",
+                    userId = 124
+                };
+
+                // Serializa el objeto a JSON
+                string jsonString = JsonConvert.SerializeObject(parametros);
+
+                var httpContent = new StringContent(jsonString, Encoding.UTF8, "aplication/json");
+                var response2 = client.PostAsync(url2, httpContent).Result;
+                var res2 = response2.Content.ReadAsStringAsync().Result;
+                dynamic r2 = JObject.Parse(res2);
+
+                Console.WriteLine(r2);
+               
+                //-POST
+            }
+
+
+
+        }
+    }
+
+ */
 
 app.UseAuthorization();
 
